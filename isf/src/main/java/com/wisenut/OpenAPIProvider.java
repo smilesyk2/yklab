12fd15b452c975eea93a34139c182a86e6631933
Holly <Holly@Holly-THINK>
2016-04-25 17:41:19 +0900

    컨텐츠 추천 방식 변경

--- isf/src/main/java/com/wisenut/OpenAPIProvider.java
+++ isf/src/main/java/com/wisenut/OpenAPIProvider.java
@@ -38,86 +38,7 @@ public class OpenAPIProvider {
 		
 		return teaWorker.getMainKeywordsInfo(article, start, pageno);
 	}
-	
-	// 1-1. Model + SF-1 결과 조합. 날짜 조건 없음. 전체 컬렉션 대상.
-	public String getRecommendedContentsInfoWithSF1Result(String article, int start, int pageno) throws Exception{
-		return getRecommendedContentsInfoWithSF1Result(article, "all", start, pageno, "", "");
-	}
-	
-	// 1-2. Model + SF-1 결과 조합. 날짜 조건 없음. 특정 컬렉션 대상.
-	public String getRecommendedContentsInfoWithSF1Result(String article, String targetCollection, int start, int pageno) throws Exception{
-		return getRecommendedContentsInfoWithSF1Result(article, targetCollection, start, pageno, "", "");
-	}
-	
-	// 1-3. Model + SF-1 결과 조합. 날짜 조건 없음. 특정 컬렉션 대상.
-	public String getRecommendedContentsInfoWithSF1Result(String article, int start, int pageno, String startDate, String endDate) throws Exception{
-		return getRecommendedContentsInfoWithSF1Result(article, "all", start, pageno, "", "");
-	}
-	
-	// 1-4. Model + SF-1 결과 조합. 날짜 조건 있음. 특정 컬렉션 대상
-	public String getRecommendedContentsInfoWithSF1Result(String articleOrWord, String targetCollection, int start, int pageno, String startDate, String endDate) throws Exception{
-		HashMap<String,ArrayList<HashMap<String,String>>> resultMap = new HashMap<String,ArrayList<HashMap<String,String>>>();
 		
-		WiseTeaWorker teaWorker = new WiseTeaWorker();
-		WiseSearchWorker searchWorker = new WiseSearchWorker();
-		
-		List<Pair<Integer>> keywordList = teaWorker.getMainKeywordsPair(articleOrWord, start, pageno);
-		
-		StringBuffer query = new StringBuffer();
-        
-        for (int i = 0; i < keywordList.size(); i++) {
-         	Pair<Integer> item = keywordList.get(i);
- 			if (null == item) {
- 				continue;
- 			}
- 			
- 			if( query.length() != 0 )
- 				query.append("|");
- 			
- 			query.append(item.key());
- 		}
-        
-        logger.debug("query : " + query.toString());
-        
-        searchWorker.search(query.toString(), pageno, startDate, endDate);
-
-        if(targetCollection.equals("all")){
-        	for(String col : collections){
-        		// 검색한 결과와 tea의 similarDoc 결과를 조합. 검색 결과 중 기사(article)의 결과만 리스트로 제공
-                List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(articleOrWord, searchWorker.getDocidMap().get(col), pageno);
-                ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
-                
-        		for(Pair<Double> p : docidList){
-        			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
-        			
-        			
-        			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
-        			if(searchWorker.getResultMap().get(col).size()>0){				
-        				resultList.add(searchWorker.getResultMap().get(col).get(0));
-        			}
-        		}
-        		resultMap.put(col, resultList);
-        	}
-        }else{
-        	// 검색한 결과와 tea의 similarDoc 결과를 조합. 검색 결과 중 기사(article)의 결과만 리스트로 제공
-            List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(articleOrWord, searchWorker.getDocidMap().get(targetCollection), pageno);
-            ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
-            
-    		for(Pair<Double> p : docidList){
-    			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
-    			
-    			
-    			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
-    			if(searchWorker.getResultMap().get(targetCollection).size()>0){				
-    				resultList.add(searchWorker.getResultMap().get(targetCollection).get(0));
-    			}
-    		}
-    		resultMap.put(targetCollection, resultList);
-    		
-        }
-		return StringUtil.objectToString(resultMap);
-	}
-	
 	// 2-1. Model. 날짜 조건 없음. 전체 컬렉션 대상.
 	public String getRecommendedContentsInfo(String article, int start, int pageno) throws Exception{
 		return getRecommendedContentsInfo(article, "all", start, pageno, "", "");
@@ -140,40 +61,100 @@ public class OpenAPIProvider {
 		WiseTeaWorker teaWorker = new WiseTeaWorker();
 		WiseSearchWorker searchWorker = new WiseSearchWorker();
         
-        // similarDoc만 사용
-        List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(article, pageno);
         
-        logger.debug("docidList size : " + docidList.size());
-        
-        if(targetCollection.equals("all")){
-        	for(String col : collections){
-        		ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
+        // 기사 길이가 200자가 넘는 경우는 Model을, 아닌 경우 SF-1+Model 결과를 조합
+        if(article.length()>200){
+        	logger.debug("article length is more than 200 characters.");
+        	
+        	// similarDoc만 사용
+        	List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(article, pageno);
+        	
+        	logger.debug("docidList size : " + docidList.size());
+        	
+        	if(targetCollection.equals("all")){
+            	for(String col : collections){
+            		ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
+                	for(Pair<Double> p : docidList){
+            			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
+            			
+            			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
+            			if(searchWorker.getResultMap().get(col).size()>0){				
+            				resultList.add(searchWorker.getResultMap().get(col).get(0));
+            			}
+            		}
+                	
+                	resultMap.put(col, resultList);
+            	}
+            }else{
+            	ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
             	for(Pair<Double> p : docidList){
         			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
         			
         			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
-        			if(searchWorker.getResultMap().get(col).size()>0){				
-        				resultList.add(searchWorker.getResultMap().get(col).get(0));
+        			if(searchWorker.getResultMap().get(targetCollection).size()>0){				
+        				resultList.add(searchWorker.getResultMap().get(targetCollection).get(0));
         			}
         		}
             	
-            	resultMap.put(col, resultList);
-        	}
+            	resultMap.put(targetCollection, resultList);
+            }
         }else{
-        	ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
-        	for(Pair<Double> p : docidList){
-    			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
-    			
-    			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
-    			if(searchWorker.getResultMap().get(targetCollection).size()>0){				
-    				resultList.add(searchWorker.getResultMap().get(targetCollection).get(0));
-    			}
-    		}
+        	logger.debug("article length is less than and equal to 200 characters.");
         	
-        	resultMap.put(targetCollection, resultList);
+        	List<Pair<Integer>> keywordList = teaWorker.getMainKeywordsPair(article, start, pageno);
+        	
+        	StringBuffer query = new StringBuffer();
+            
+            for (int i = 0; i < keywordList.size(); i++) {
+             	Pair<Integer> item = keywordList.get(i);
+     			if (null == item) {
+     				continue;
+     			}
+     			
+     			if( query.length() != 0 )
+     				query.append("|");
+     			
+     			query.append(item.key());
+     		}
+            
+            logger.debug("query : " + query.toString());
+            
+            searchWorker.search(query.toString(), pageno, startDate, endDate);
+            
+            if(targetCollection.equals("all")){
+            	for(String col : collections){
+            		// 검색한 결과와 tea의 similarDoc 결과를 조합. 검색 결과 중 기사(article)의 결과만 리스트로 제공
+                    List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(article, searchWorker.getDocidMap().get(col), pageno);
+                    ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
+                    
+            		for(Pair<Double> p : docidList){
+            			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
+            			
+            			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
+            			if(searchWorker.getResultMap().get(col).size()>0){				
+            				resultList.add(searchWorker.getResultMap().get(col).get(0));
+            			}
+            		}
+            		resultMap.put(col, resultList);
+            	}
+            }else{
+            	// 검색한 결과와 tea의 similarDoc 결과를 조합. 검색 결과 중 기사(article)의 결과만 리스트로 제공
+                List<Pair<Double>> docidList = teaWorker.getRecommendedContentsPair(article, searchWorker.getDocidMap().get(targetCollection), pageno);
+                ArrayList<HashMap<String,String>> resultList = new ArrayList<HashMap<String,String>>();
+                
+        		for(Pair<Double> p : docidList){
+        			searchWorker.docidSearch(p.key(), pageno, startDate, endDate);
+        			
+        			
+        			// DOCID Search에 대한 결과는 한 개이므로 첫번째 결과만 가져와서 add.
+        			if(searchWorker.getResultMap().get(targetCollection).size()>0){				
+        				resultList.add(searchWorker.getResultMap().get(targetCollection).get(0));
+        			}
+        		}
+        		resultMap.put(targetCollection, resultList);
+            }
         }
-		
-		
+        
 		return StringUtil.objectToString(resultMap);
 	}
 	
@@ -216,8 +197,10 @@ public class OpenAPIProvider {
 			System.out.println(strResultData);
 			
 			System.out.println("================================================================================================================== 내부 API");
-			String article = "겨울채비 하세요... 이번 주 맑고 포근 윤우현 기자 whyoon@jbnews.com 11월4째주 충북지방은 대체로 고기압의 영향을 받아 맑겠으며, 기온도 평년보다 조금 높아 포근한 한 주가 될 것으로 전망된다.청주기상대는 20일 충북지방은 고기압의 영향을 받아 맑은 날씨를 보이겠으며, 아침최저 청주2도, 충주 영하1도 등 영하2~영상2도의 분포를 보이겠고, 낮 최고기온은 청주 16도, 충주 14도 등 14~16도로 포근할 것이라고 예보했다. 한편 11월 셋째 휴일인 19일 충북지역은 구름이 낀 흐린 날씨를 보인 가운데 유명산 등에는 다소 한산한 모습을 보였다. 가을 단풍이 모두 떨어진 월악산국립공원 입장객은 2천명으로 지난 주의 절반에 그쳤고 속리산에는 4천명의 등산객이 산행을 즐겼으나 평소보다 적은 수준이었다. 대통령 옛 별장인 청원 청남대에도 휴일 평균 관람객의 50%를 밑도는 2천명만 입장해 초겨울 대청호의 풍광을 감상했다. 청주 상당산성, 청원 문의문화재 단지 등 도내 주요 유원지에서는 두툼한 옷을 입은 가족단위 행락객이 등산, 산책, 외식 등을 하며 휴일의 여유를 즐겼다. 청주 도심 극장가는 대입 수능을 마친 고3학생 등으로 북적거렸고 도시와 농촌가정에서는 김장김치를 담그는 손길이 분주했다."; 
+			//String article = "겨울채비 하세요... 이번 주 맑고 포근 윤우현 기자 whyoon@jbnews.com 11월4째주 충북지방은 대체로 고기압의 영향을 받아 맑겠으며, 기온도 평년보다 조금 높아 포근한 한 주가 될 것으로 전망된다.청주기상대는 20일 충북지방은 고기압의 영향을 받아 맑은 날씨를 보이겠으며, 아침최저 청주2도, 충주 영하1도 등 영하2~영상2도의 분포를 보이겠고, 낮 최고기온은 청주 16도, 충주 14도 등 14~16도로 포근할 것이라고 예보했다. 한편 11월 셋째 휴일인 19일 충북지역은 구름이 낀 흐린 날씨를 보인 가운데 유명산 등에는 다소 한산한 모습을 보였다. 가을 단풍이 모두 떨어진 월악산국립공원 입장객은 2천명으로 지난 주의 절반에 그쳤고 속리산에는 4천명의 등산객이 산행을 즐겼으나 평소보다 적은 수준이었다. 대통령 옛 별장인 청원 청남대에도 휴일 평균 관람객의 50%를 밑도는 2천명만 입장해 초겨울 대청호의 풍광을 감상했다. 청주 상당산성, 청원 문의문화재 단지 등 도내 주요 유원지에서는 두툼한 옷을 입은 가족단위 행락객이 등산, 산책, 외식 등을 하며 휴일의 여유를 즐겼다. 청주 도심 극장가는 대입 수능을 마친 고3학생 등으로 북적거렸고 도시와 농촌가정에서는 김장김치를 담그는 손길이 분주했다."; 
+			String article = "청주 도심 극장가는 대입 수능을 마친 고3학생 등으로 북적거렸고 도시와 농촌가정에서는 김장김치를 담그는 손길이 분주했다.";
 			System.out.println("###### Input Article : "+ article);
+			System.out.println("###### ( article length is "+ article.length() + ".)");
 			
 			strResultData = provider.getMainKeywordsInfo(article, 0, 10);
 			System.out.println("#1. 키워드(해쉬태그) 추출 : " + strResultData);
@@ -233,18 +216,6 @@ public class OpenAPIProvider {
 			
 			strResultData = provider.getRecommendedContentsInfo(article, "video", 0, 10, "20150101", "20151231");
 			System.out.println("#2-4. 연관기사 추천. 특정 컬렉션. 날짜 조건 O : " + strResultData);
-			
-			strResultData = provider.getRecommendedContentsInfoWithSF1Result(article, 0, 10);
-			System.out.println("#3-1. SF-1 검색 결과를 조합한 연관기사 추천. 전체 컬렉션. 날짜 조건 X : " + strResultData);
-			
-			strResultData = provider.getRecommendedContentsInfoWithSF1Result(article, "image", 0, 10);
-			System.out.println("#3-2. SF-1 검색 결과를 조합한 연관기사 추천. 특정 컬렉션. 날짜 조건 X : " + strResultData);
-			
-			strResultData = provider.getRecommendedContentsInfoWithSF1Result(article, 0, 10, "20140101", "20141231");
-			System.out.println("#3-3. SF-1 검색 결과를 조합한 연관기사 추천. 전체 컬렉션. 날짜 조건 O : " + strResultData);
-			
-			strResultData = provider.getRecommendedContentsInfoWithSF1Result(article, "article", 0, 10, "20140101", "20141231");
-			System.out.println("#3-4. SF-1 검색 결과를 조합한 연관기사 추천. 특정 컬렉션. 날짜 조건 O : " + strResultData);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
